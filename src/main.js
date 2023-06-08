d3a6390181c9cd52ffee0b2a3a7f96c473879b32
Megalith <gekocakaya@gmail.com>
2023-06-08 16:21:46 +0300

    trying smth

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Towns, Nations, Player, Server, Sieges } from "./export.js";
-import types  from "./types/types.js";
+import types from "./types/types.js";
 import { RequestManager } from "./request/RequestManager.js";
 import { Functions } from "./utils/functions.js";
 
@@ -10,7 +10,7 @@ export class CCnet {
    * @param {object} options 
    */
   constructor(options = { debug: false, RequestManager: {}, Functions: {} }) {
-   this.init(options);
+    this.init(options);
   };
 
   /**
@@ -19,14 +19,46 @@ export class CCnet {
    * @returns {void}
    */
   init(options) {
+    /**
+     * @param {import("./export.js").Player} player
+     */
     this.player = new Player(this, options);
+
+    /**
+     * @param {import("./export.js").Nations} nations
+     */
     this.nations = new Nations(this, options);
+
+    /**
+     * @param {import("./export.js").Server} server
+     */
     this.server = new Server(this, options);
+
+    /**
+     * @param {import("./export.js").Sieges} Sieges
+     */
     this.siege = new Sieges(this, options);
+
+    /**
+     * @param {import("./export.js").Towns} towns
+     */
     this.towns = new Towns(this, options);
+
+    /**
+     * @param {import("./export.js").RequestManager} RequestManager
+     */
     this.RequestManager = new RequestManager(options.RequestManager);
+
+    /**
+     * @param {import("./export.js").Functions} Functions
+     */
     this.Functions = new Functions(options.Functions);
+
+    /**
+     * @param {types} types
+     */
     this.types = types;
-   return void 0;
+    
+    return void 0;
   };
 };
